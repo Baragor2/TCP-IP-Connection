e2d21c584cfa0f03714bf9f70eefd86de858bc3a
Yauheni Mukhin <145256155+Baragor2@users.noreply.github.com>
2024-05-05 13:37:59 +0300

    functional

--- client.py
+++ client.py
@@ -3,11 +3,21 @@ import socket
 HOST = "localhost"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((HOST, PORT))
 
-    with open('cat.jpg', mode="rb") as file:
-        data = file.read(2048)
-        while data:
-            client.send(data)
-            data = file.read(2048)
+
+def send_file(file_name: str, host: str, port: int) -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((host, port))
+        client.send(bytes(file_name, encoding='utf-8'))
+
+        with open(file_name, 'rb') as file:
+            client.sendfile(file)
+
+
+def main() -> None:
+    file_name = input()
+    send_file(file_name, HOST, PORT)
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -2,21 +2,47 @@ import socket
 
 HOST = 'localhost'
 PORT = 65432
+BUFFER_SIZE = 2048
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
 
-print(HOST)
-print('waiting...')
+def make_connection(host: str, port: int) -> socket:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    return server
 
-client_socket, client_address = server.accept()
 
-print(f'{client_address} connected')
+def communicate_with_client(host: str) -> None:
+    print(host)
+    print('waiting...')
 
-with open('image_server.jpg', mode="wb") as file:
-    data = client_socket.recv(2048)
-    while data:
-        file.write(data)
-        data = client_socket.recv(2048)
-server.close()
+
+def get_client(server: socket) -> tuple[socket, socket]:
+    client_socket, client_address = server.accept()
+    print(f'{client_address} connected')
+    return client_socket, client_address
+
+
+def get_file_name(client_socket: socket, buffer: int) -> str:
+    file_name = 'server_' + client_socket.recv(buffer).decode()
+    return file_name
+
+
+def download_file(file_name: str, client_socket: socket, buffer: int) -> None:
+    with open(file_name, mode="wb") as file:
+        data = client_socket.recv(buffer)
+        while data:
+            file.write(data)
+            data = client_socket.recv(buffer)
+
+
+def main():
+    server = make_connection(HOST, PORT)
+    communicate_with_client(HOST)
+    client_socket, client_address = get_client(server)
+    file_name = get_file_name(client_socket, BUFFER_SIZE)
+    download_file(file_name, client_socket, BUFFER_SIZE)
+
+
+if __name__ == '__main__':
+    main()
